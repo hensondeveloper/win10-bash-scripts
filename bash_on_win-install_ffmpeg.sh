a437c5c15771bf0eac316d0016263920f0e9e2fb
hensondeveloper <henson.developer@outlook.com>
2016-04-20 01:15:43 +0000

    First script!

---
+++ bash_on_win-install_ffmpeg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+which curl
+if [ $? -eq 0 ]; then	
+    # Install ffmpeg from the trusty-media PPA
+    add-apt-repository -y ppa:mc3man/trusty-media
+    apt-get update
+    apt-get -y install ffmpeg
+
+    # Install and link youtube-dl
+    curl https://yt-dl.org/downloads/2016.04.19/youtube-dl -o /usr/local/bin/youtube-dl
+    chmod a+rx /usr/local/bin/youtube-dl
+else 
+    echo "Please install curl before running this script."; 
+fi
